4c560403bed3b55747a2a68a04edb8de724d9703
Gregory Thompson <devalidating@gmail.com>
2020-01-12 15:27:40 -0800

    Basic temp explorations

--- Chat App/src/main/java/main.java
+++ Chat App/src/main/java/main.java
@@ -1,6 +1,33 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 class main {
 
     public static void main(String[] args) {
         System.out.println("works");
+        // does it really
+        
+        try {
+            ServerSocket serv = new ServerSocket(50000);
+            Socket usr = serv.accept();
+            InputStream input = usr.getInputStream();
+            byte[] message = new byte[25];
+            while(true)
+            {
+                
+                //System.out.println(input.available());
+                if(input.available()>0)
+                {
+                    input.read(message);
+                    String inMessage = new String(message);
+                    System.out.println(inMessage);
+                }
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
