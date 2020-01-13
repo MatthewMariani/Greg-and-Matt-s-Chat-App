cd439445d8d5271e747f369dde807017d4eb979a
Gregory Thompson <devalidating@gmail.com>
2020-01-12 19:40:41 -0800

    created simple functional http server

--- Chat App/src/main/java/main.java
+++ Chat App/src/main/java/main.java
@@ -1,35 +1,55 @@
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.http.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 class main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("works");
-        // does it really
+        final ServerSocket serv = new ServerSocket(8009);
+        final Socket usr = serv.accept();
+        System.out.println("connected");
+        final OutputStream outputStream = usr.getOutputStream();
+        final PrintWriter out = new PrintWriter(outputStream);
+        File index = new File("Chat App\\files\\index.html");
+        out.println("HTTP/1.0 200 OK");
+        //out.print("Content-Type: text/html");
+        String content = Files.readString(Paths.get("Chat App\\files\\index.html"));
+        out.println("Content-Length: "+content.length());
+        out.println("");
+        
+        System.out.println(content);
+        out.println(content);
+        out.flush();
+        System.out.println("sent");
+
+    }
+
+    static String readFile(File input) {
+
+        String out = "";
         
         try {
-            ServerSocket serv = new ServerSocket(8080);
-            Socket usr = serv.accept();
-            InputStream input = usr.getInputStream();
-            byte[] message = new byte[25];
-            while(true)
+            Scanner temp = new Scanner(input);
+            while(temp.hasNextLine())
             {
-
-                
-                //System.out.println(input.available());
-                if(input.available()>0)
-                {
-                    input.read(message);
-                    String inMessage = new String(message);
-                    System.out.println(inMessage);
-                }
+                out+=temp.nextLine()+"\n";
             }
-        } catch (IOException e) {
+        } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        return out;
+
     }
 }
