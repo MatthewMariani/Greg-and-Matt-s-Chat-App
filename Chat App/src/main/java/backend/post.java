b9ad9bffd8cfcd349826956286992ea7102ffc8a
Matthew Mariani <mmariani20@siprep.org>
2020-01-22 21:06:34 -0800

    modified post.java and added reply.java

--- Chat App/src/main/java/backend/post.java
+++ Chat App/src/main/java/backend/post.java
@@ -7,7 +7,11 @@ public class post
     //date/time object? for date var
     //post-body?
     //attached-image?
-    private String posterID;
+    private User poster;
+    private ArrayList<reply> replies = new ArrayList<reply>();
+    //ways to measure engagement/popularity: views, comments, likes/upvotes?(total # would be invisible to users)
 
-    //idea: include a toHTML function to format user posts
+    //idea: include a toHTML function to format user posts or a toSQLFormat method for database storage
+    //toDisplayableID
+    //sorting: by replies/likes/views{aka popularity}, by most recent post{postdate}, 
 }
---
+++ Chat App/src/main/java/backend/reply.java
@@ -0,0 +1,12 @@
+package main.java.backend;
+
+//reply object to be stored in ArrayList<reply> replies in post.java
+public class reply
+{
+    //private date postdate;
+    //private post-body postbody
+    private User poster;
+    //attached-image?
+    //likes/upvotes?
+
+}
